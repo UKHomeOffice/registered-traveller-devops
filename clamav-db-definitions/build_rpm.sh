6810e6878091f7dad41a2292f2a5dcb13674b855
Jay Keshur <jaykeshur.homeoffice@gmail.com>
2015-08-25 14:46:25 +0100

    Changing name of rpm

--- clamav-db-definitions/build_rpm.sh
+++ clamav-db-definitions/build_rpm.sh
@@ -8,13 +8,13 @@ function fail() {
 DATE=`date "+%Y%m%d_%H%M"`
 
 rm -rf fpm/*
-mkdir -p fpm/var/lib/clamav
+mkdir -p fpm/var/lib/clamav-db-definitions
 
-wget http://database.clamav.net/main.cvd -P fpm/var/lib/clamav || fail "Failed to download cvd"
-wget http://database.clamav.net/daily.cvd -P fpm/var/lib/clamav || fail "Failed to download cvd"
-wget http://database.clamav.net/bytecode.cvd -P fpm/var/lib/clamav || fail "Failed to download cvd"
+wget http://database.clamav.net/main.cvd -P fpm/var/lib/clamav-db-definitions || fail "Failed to download cvd"
+wget http://database.clamav.net/daily.cvd -P fpm/var/lib/clamav-db-definitions || fail "Failed to download cvd"
+wget http://database.clamav.net/bytecode.cvd -P fpm/var/lib/clamav-db-definitions || fail "Failed to download cvd"
 
 fpm -C fpm -t rpm -s dir \
     -p NAME-VERSION.ARCH.TYPE \
-    -n clam-av-definitions -v "$DATE" -a all . \
+    -n clamav-db-definitions -v "$DATE" -a all . \
     || fail "Failed to build rpm"
