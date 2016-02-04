51b811ab710a428284a6793ea3f5fe420e826ed9
Jay Keshur <jaykeshur.homeoffice@gmail.com>
2016-02-04 12:02:51 +0000

    Change perms of cvd files

--- clamav-db-definitions/build_rpm.sh
+++ clamav-db-definitions/build_rpm.sh
@@ -14,6 +14,8 @@ wget http://database.clamav.net/main.cvd -P fpm/var/lib/clamav-db-definitions ||
 wget http://database.clamav.net/daily.cvd -P fpm/var/lib/clamav-db-definitions || fail "Failed to download cvd"
 wget http://database.clamav.net/bytecode.cvd -P fpm/var/lib/clamav-db-definitions || fail "Failed to download cvd"
 
+chmod 644 fpm/var/lib/clamav-db-definitions/*
+
 fpm -C fpm -t rpm -s dir \
     -p NAME-VERSION.ARCH.TYPE \
     -n clamav-db-definitions -v "$DATE" -a all . \
